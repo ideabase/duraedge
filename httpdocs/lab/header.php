1fa86a09d662d4b3b1d1eb6e1697ab398d609fcc
Alex Longwell <alongwell@ideabasekent.com>
2017-12-13 14:28:36 -0500

    header/footer template divs and baseball image

--- httpdocs/lab/header.php
+++ httpdocs/lab/header.php
@@ -2,10 +2,10 @@
   <a href="#" class="header__logo"><img src="../assets/img/ui/DuraEdgeWordmark.svg" alt="DuraEdge" class="header__logo__img"/></a>
   <nav class="header__nav">
      <ul class="header__nav__list">
-         <li class="nav__list__item"><a href="#">Why DuraEdge?</a></li>
-         <li class="nav__list__item"><a href="#">Our Products</a></li>
-         <li class="nav__list__item"><a href="#">Field Maintenance</a></li>
-         <li class="nav__list__item"><a href="#">Contact DuraEdge</a></li>
+         <li class="nav__list__item"><a href="#"><img src="../assets/img/ui/baseball.svg" alt="baseball" class="nav__list__baseball"/>Why DuraEdge?</a></li>
+         <li class="nav__list__item"><a href="#"><img src="../assets/img/ui/baseball.svg" alt="baseball" class="nav__list__baseball"/>Our Products</a></li>
+         <li class="nav__list__item"><a href="#"><img src="../assets/img/ui/baseball.svg" alt="baseball" class="nav__list__baseball"/>Field Maintenance</a></li>
+         <li class="nav__list__item"><a href="#"><img src="../assets/img/ui/baseball.svg" alt="baseball" class="nav__list__baseball"/>Contact DuraEdge</a></li>
      </ul>
   </nav>
   <nav class="header__secondnav">
