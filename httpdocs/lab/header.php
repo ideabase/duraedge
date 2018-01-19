750334a6f6a2461fe1e7c0fd130d3917953ff5ec
Alex Longwell <alongwell@ideabasekent.com>
2018-01-19 14:37:33 -0500

    subnav styling

--- httpdocs/lab/header.php
+++ httpdocs/lab/header.php
@@ -1,5 +1,10 @@
 <header>
-  <a href="#" class="header__logo"><img src="../assets/img/ui/DuraEdgeWordmark.svg" alt="DuraEdge" class="header__logo__img"/></a>
+  <a href="#" class="header__logo">
+    <picture>
+      <source srcset="../assets/img/ui/duraedgelogo.svg" media="(min-width: 750px)">
+      <img src="../assets/img/ui/DuraEdgeWordmark.svg" alt="DuraEdge" class="header__logo__img"/>
+    </picture>
+  </a>
   <nav class="header__nav">
      <ul class="header__nav__list">
          <li class="nav__list__item"><a href="#"><img src="../assets/img/ui/baseball.svg" alt="baseball" class="nav__list__baseball"/>Why DuraEdge?</a></li>
@@ -11,8 +16,8 @@
   <nav class="header__secondnav">
     <ul class="header__nav__sublist">
       <li class="nav__sublist__item"><a href="#">About DuraEdge</a></li>
-      <li class="nav__sublist__item"><a href="#">Certification Programs</a></li>
-      <li class="nav__sublist__item"><a href="#">For Architects</a></li>
+      <li class="nav__sublist__item"><a href="#">Certifications</a></li>
+      <li class="nav__sublist__item"><a href="#">Architects</a></li>
       <li class="nav__sublist__item"><a href="#">Find DuraEdge</a></li>
     </ul>
   </nav>
