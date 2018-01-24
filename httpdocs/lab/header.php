08ebcabd85ad3158ad3abf8d4e29f15f46b44f94
Alex Longwell <alongwell@ideabasekent.com>
2018-01-24 10:53:02 -0500

    Header/Footer CSS changes

--- httpdocs/lab/header.php
+++ httpdocs/lab/header.php
@@ -1,24 +1,27 @@
 <header>
-  <a href="#" class="header__logo">
-    <picture>
-      <source srcset="../assets/img/ui/duraedgelogo.svg" media="(min-width: 750px)">
-      <img src="../assets/img/ui/DuraEdgeWordmark.svg" alt="DuraEdge" class="header__logo__img"/>
-    </picture>
-  </a>
-  <nav class="header__nav">
-     <ul class="header__nav__list">
-         <li class="nav__list__item"><a href="#"><img src="../assets/img/ui/baseball.svg" alt="baseball" class="nav__list__baseball"/>Why DuraEdge?</a></li>
-         <li class="nav__list__item"><a href="#"><img src="../assets/img/ui/baseball.svg" alt="baseball" class="nav__list__baseball"/>Our Products</a></li>
-         <li class="nav__list__item"><a href="#"><img src="../assets/img/ui/baseball.svg" alt="baseball" class="nav__list__baseball"/>Field Maintenance</a></li>
-         <li class="nav__list__item"><a href="#"><img src="../assets/img/ui/baseball.svg" alt="baseball" class="nav__list__baseball"/>Contact DuraEdge</a></li>
-     </ul>
-  </nav>
-  <nav class="header__secondnav">
-    <ul class="header__nav__sublist">
-      <li class="nav__sublist__item"><a href="#">About DuraEdge</a></li>
-      <li class="nav__sublist__item"><a href="#">Certifications</a></li>
-      <li class="nav__sublist__item"><a href="#">Architects</a></li>
-      <li class="nav__sublist__item"><a href="#">Find DuraEdge</a></li>
-    </ul>
-  </nav>
+  <div class="header__wrapper">
+    <a href="#" class="header__logo">
+      <picture>
+        <source srcset="../assets/img/ui/duraedgelogo.svg" media="(min-width: 750px)">
+        <img src="../assets/img/ui/DuraEdgeWordmark.svg" alt="DuraEdge" class="header__logo__img"/>
+      </picture>
+    </a>
+    <nav class="header__nav">
+       <ul class="header__nav__list">
+           <li class="nav__list__item"><a href="#">Home</a></li>
+           <li class="nav__list__item"><a href="#">Why DuraEdge?</a></li>
+           <li class="nav__list__item"><a href="#">Our Products</a></li>
+           <li class="nav__list__item"><a href="#">Field Maintenance</a></li>
+           <li class="nav__list__item"><a href="#">Contact DuraEdge</a></li>
+       </ul>
+    </nav>
+    <nav class="header__secondnav">
+      <ul class="header__nav__sublist">
+        <li class="nav__sublist__item"><a href="#">About DuraEdge</a></li>
+        <li class="nav__sublist__item"><a href="#">Certifications</a></li>
+        <li class="nav__sublist__item"><a href="#">Architects</a></li>
+        <li class="nav__sublist__item"><a href="#">Find DuraEdge</a></li>
+      </ul>
+    </nav>
+  </div>
 </header>
