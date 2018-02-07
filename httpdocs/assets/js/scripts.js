1f7eb155ec142e507a1844e8359e09c66184b744
Nick Pappas <npappas@ideabasekent.com>
2018-02-07 15:21:05 -0500

    owl carousel added and attempted scripting for image scroll with page — in progress

--- httpdocs/assets/js/scripts.js
+++ httpdocs/assets/js/scripts.js
@@ -1,1 +1,22 @@
 // JavaScript Goes Here //
+
+/*
+$(function() {
+    var photo = $(".feature__image");
+    $('.content').scroll(function() {
+        var scroll = $('.content').scrollTop();
+
+        if (scroll >= 200) {
+
+            photo.removeClass('imageNUMBER').addClass("image1");
+        } else {
+            photo.removeClass("image1").addClass('image0');
+        }
+    });
+});
+*/
+
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel();
+});
